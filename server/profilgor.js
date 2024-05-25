ef1db2ddb60a5775eddfabf8996c48b072acc2a9
betullsvs <betullsvss21@gmail.com>
2024-05-25 14:42:08 +0300

    profilgor

---
+++ server/profilgor.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const User = require('./mongo');  
+r
+router.get('/', async (req, res) => {
+    try {
+        console.log('Fetching users...'); 
+        const users = await User.User.find({ isDietitian: false });
+        console.log('Fetched users:', users); 
+        res.status(200).json(users); 
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+module.exports = router;
