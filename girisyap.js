96f2be99b3f4ddce373a9d089eb95d2c3800d2f8
umuty <umuty@DESKTOP-OJSI3TQ>
2024-05-25 20:36:19 +0300

    for switch

--- girisyap.js
+++ girisyap.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require('./mongo'); 
+const { User } = require('./mongo');
+const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res) => {
     const { phone, password } = req.body;
     console.log("Formdan gelen veriler:", phone, password);
 
     try {
-        const user = await User.findOne({ phone, password }); 
-        if (user) {
+        const user = await User.findOne({ phone, password });
+
+        const isPasswordValid = await comparePassword(password, user.password);
+        if(!isPasswordValid){
+            return res.status(401).send('Geçersiz parola');
+        }
+
+        else if (user) {
             console.log("Giriş başarılı:", user);
             if (user.isDietitian) {
                 res.redirect('/diyetisyen.html');
